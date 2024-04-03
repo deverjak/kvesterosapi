851f047cba59e743028197fd24c47924822c4af4
Jakub Devera <kuba.devera@seznam.cz>
2024-04-03 21:13:18 +0200

    Remove unused functionality

--- KvesterosApi/Program.cs
+++ KvesterosApi/Program.cs
@@ -54,25 +54,4 @@ app.MapControllers();
 
 app.UseCors("AllowEverything");
 
-// Add test data
-/*using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    AddTestData(context);
-}*/
-
 app.Run();
-
-
-
-void AddTestData(ApplicationDbContext context)
-{
-    var testHike1 = new Hike { Id = 1, Name = "Hike 1", Description = "Description 1", DifficultyLevel = DifficultyLevel.Moderate, Distance = 10, Route = "Route 1" };
-    var testHike2 = new Hike { Id = 2, Name = "Hike 2", Description = "Description 2", DifficultyLevel = DifficultyLevel.Hard, Distance = 20, Route = "Route 2" };
-    var testHike3 = new Hike { Id = 3, Name = "Hike 3", Description = "Description 3", DifficultyLevel = DifficultyLevel.Easy, Distance = 5, Route = "Route 3" };
-    var testHike4 = new Hike { Id = 4, Name = "Hike 4", Description = "Description 3", DifficultyLevel = DifficultyLevel.Uknown, Distance = 8, Route = "Route 4" };
-
-    context.Hikes.AddRange(testHike1, testHike2, testHike3, testHike4);
-    context.SaveChanges();
-}
